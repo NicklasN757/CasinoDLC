73c2cc00c8ea5d72bdcee35fae3661e9558b3024
Gronne <kasperg102@gmail.com>
2019-10-28 12:48:55 +0100

    TeamBracketOnTheWay

--- CasinoDLCApp/CasinoDLCApp/Program.cs
+++ CasinoDLCApp/CasinoDLCApp/Program.cs
@@ -6,25 +6,34 @@ namespace CasinoDLCApp
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
+            TeamBracketBetting();
             //Det antal coins du starter med. 
             int playerCasinoCoins = 10000;
             Console.WriteLine("Du har " + playerCasinoCoins + " Coins lige nu.");
             Console.ReadLine();
 
 
-=======
             int casinCoins = 10000;
 
 
             static void TeamBracketBetting()
             {
-                int numberOfTeams = 0;
+                int arrayCounter = 0; //Used to count arrays.
+
                 Console.WriteLine("Hello! You have chosen to bet on Team Brackets! Please tell me how many teams are in the bracket!");
-                numberOfTeams = int.Parse(Console.ReadLine());
-                string[] teamNames = string[numberOfTeams];
+                int numberOfTeams = int.Parse(Console.ReadLine());
+
+                string[] teamNames = new string[numberOfTeams];
+                Console.WriteLine("Now! Please tell me the names of the teams!");
+                while (arrayCounter < numberOfTeams)
+                {
+                    teamNames[arrayCounter] = Console.ReadLine();
+                    arrayCounter++;
+                }
+                arrayCounter = 0;
+                Console.WriteLine("Ty! Now, please tell me how many people are betting!");
+                int numberOfPeopleBetting = int.Parse(Console.ReadLine());
             }
->>>>>>> Stashed changes
         }
     }
 }
