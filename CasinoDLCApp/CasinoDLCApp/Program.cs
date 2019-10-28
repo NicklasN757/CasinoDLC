498d2be83f69cf74df5dea020a3d1ff2b1901668
Gronne <kasperg102@gmail.com>
2019-10-28 12:32:36 +0100

    Auto stash before merge of "master" and "origin/master"

--- CasinoDLCApp/CasinoDLCApp/Program.cs
+++ CasinoDLCApp/CasinoDLCApp/Program.cs
@@ -6,12 +6,25 @@ namespace CasinoDLCApp
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             //Det antal coins du starter med. 
             int playerCasinoCoins = 10000;
             Console.WriteLine("Du har " + playerCasinoCoins + " Coins lige nu.");
             Console.ReadLine();
 
 
+=======
+            int casinCoins = 10000;
+
+
+            static void TeamBracketBetting()
+            {
+                int numberOfTeams = 0;
+                Console.WriteLine("Hello! You have chosen to bet on Team Brackets! Please tell me how many teams are in the bracket!");
+                numberOfTeams = int.Parse(Console.ReadLine());
+                string[] teamNames = string[numberOfTeams];
+            }
+>>>>>>> Stashed changes
         }
     }
 }
