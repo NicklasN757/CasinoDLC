f5a0bfdfaa6186d375d1da39ff74f5e1558b61b4
uuu <498801920@qq.com>
2019-10-28 13:31:10 +0100

    Program fix

--- CasinoDLCApp/CasinoDLCApp/Program.cs
+++ CasinoDLCApp/CasinoDLCApp/Program.cs
@@ -8,16 +8,10 @@ namespace CasinoDLCApp
         {
             int playerCasinoCoins = 10000; //Coins du starter med. 
             bool isRunning = true;
-        static void teamBetting()
-        {
-            Console.WriteLine("Ready for lose all your gold or become a God of betting");
-            Console.ReadKey();
-            Console.Clear();
-            string redteam = "Red Team";
-            string blueteam = "Blue Team";
-            int Pool = 0;
 
-            while (isRunning) { 
+
+            while (isRunning)
+            {
                 Console.WriteLine("Du har " + playerCasinoCoins + " Coins lige nu.");
                 Console.WriteLine();
                 Console.WriteLine("0. Lukker dette program.");
@@ -30,32 +24,6 @@ namespace CasinoDLCApp
                 Console.Write("Vælg det spil du vil spille: ");
                 string reply = Console.ReadLine();
                 Console.Clear();
-            //=========================================================================================================
-            Console.Write("How many people bet on Red Team: "); int numberofpeople1 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            int o = 0;
-            int number = o + 1;
-            int[] goldarray = new int[numberofpeople1];
-            while (o < numberofpeople1)
-            {
-                Console.Write("Player "+ number +": ");
-                goldarray[o] = int.Parse(Console.ReadLine());
-                Pool += goldarray[o];
-                o++;
-            }
-            //=========================================================================================================
-            Console.Write("How many people bet on Blue Team: "); int numberofpeople2 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            int p = 0;
-            int number2 = p + 1;
-            int[] goldarray2 = new int[numberofpeople2];
-            while (p < numberofpeople2)
-            {
-                Console.Write("Player " + number2 + ": ");
-                goldarray[p] = int.Parse(Console.ReadLine());
-                Pool += goldarray[p];
-                o++;
-            }
 
                 if (reply == "0")
                 {
@@ -103,10 +71,45 @@ namespace CasinoDLCApp
                 Console.WriteLine("Ty! Now, please tell me how many people are betting!");
                 int numberOfPeopleBetting = int.Parse(Console.ReadLine());
             }
+
+            static void teamBetting()
+            {
+                Console.WriteLine("Ready for lose all your gold or become a God of betting");
+                Console.ReadKey();
+                Console.Clear();
+                string redteam = "Red Team";
+                string blueteam = "Blue Team";
+                int Pool = 0;
+                //=========================================================================================================
+                Console.Write("How many people bet on Red Team: "); int numberofpeople1 = int.Parse(Console.ReadLine());
+                Console.Clear();
+                int o = 0;
+                int number = o + 1;
+                int[] goldarray = new int[numberofpeople1];
+                while (o < numberofpeople1)
+                {
+                    Console.Write("Player " + number + ": ");
+                    goldarray[o] = int.Parse(Console.ReadLine());
+                    Pool += goldarray[o];
+                    o++;
+                }
+                //=========================================================================================================
+                Console.Write("How many people bet on Blue Team: "); int numberofpeople2 = int.Parse(Console.ReadLine());
+                Console.Clear();
+                int p = 0;
+                int number2 = p + 1;
+                int[] goldarray2 = new int[numberofpeople2];
+                while (p < numberofpeople2)
+                {
+                    Console.Write("Player " + number2 + ": ");
+                    goldarray[p] = int.Parse(Console.ReadLine());
+                    Pool += goldarray[p];
+                    o++;
+                }
+            }
+
+
         }
 
-
     }
-
-
 }
