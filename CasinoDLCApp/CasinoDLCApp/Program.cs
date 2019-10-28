e160e6151ba5a4685b3f9053d926f5cac75a5511
Nicklas Markvart Nielsen <1n2n3n4n5n@hotmail.dk>
2019-10-28 12:59:59 +0100

    Menu added

--- CasinoDLCApp/CasinoDLCApp/Program.cs
+++ CasinoDLCApp/CasinoDLCApp/Program.cs
@@ -6,11 +6,49 @@ namespace CasinoDLCApp
     {
         static void Main(string[] args)
         {
-            //Det antal coins du starter med. 
-            int playerCasinoCoins = 10000;
-            Console.WriteLine("Du har " + playerCasinoCoins + " Coins lige nu.");
-            Console.ReadLine();
+            int playerCasinoCoins = 10000; //Coins du starter med. 
+            bool isRunning = true;
 
+            while (isRunning) { 
+                Console.WriteLine("Du har " + playerCasinoCoins + " Coins lige nu.");
+                Console.WriteLine();
+                Console.WriteLine("0. Lukker dette program.");
+                Console.WriteLine("1. Team Bracket Betting.");
+                Console.WriteLine("2. 1v1 Team Betting.");
+                Console.WriteLine("3. Slot Maskine.");
+                Console.WriteLine("4. Blackjack.");
+                Console.WriteLine("5. Spin The Wheel.");
+                Console.WriteLine();
+                Console.Write("Vælg det spil du vil spille: ");
+                string reply = Console.ReadLine();
+                Console.Clear();
+
+                if (reply == "0")
+                {
+                    Console.WriteLine("Du har valgt nummeret " + reply + " programmet vil nu lukke.......");
+                    isRunning = false;
+                }
+                else if (reply == "1")
+                {
+                    Console.WriteLine("Du har valgt nummer " + reply + ". Du spiller nu \"Team Bracket Betting\".");
+                }
+                else if (reply == "2")
+                {
+                    Console.WriteLine("Du har valgt nummer " + reply + ". Du spiller nu \"1v1 Team Betting\".");
+                }
+                else if (reply == "3")
+                {
+                    Console.WriteLine("Du har valgt nummer " + reply + ". Du spiller nu \"Slot Maskine\".");
+                }
+                else if (reply == "4")
+                {
+                    Console.WriteLine("Du har valgt nummer " + reply + ". Du spiller nu \"Blackjack\".");
+                }
+                else if (reply == "5")
+                {
+                    Console.WriteLine("Du har valgt nummer " + reply + ". Du spiller nu \"Spin The Wheel\".");
+                }
+            }
 
         }
     }
